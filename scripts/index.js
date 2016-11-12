4d3fae468dd2885e9ab5eece7e86f7fd161d782f
pixels-bytes <adam@pixelsandbytes.co>
2016-11-12 23:12:20 +0100

    All items got from Contentful

--- scripts/index.js
+++ scripts/index.js
@@ -19,17 +19,42 @@
 
 
 // THE REQUIREMENTS
-const fs = require('fs');
-const pug = require('pug');
-const dotenv = require('dotenv').config();
+const dotenv  = require('dotenv').config();
+const fs      = require('fs');
+const pug     = require('pug');
+const request = require('request');
+
+
+// THE REQUEST
+const apiKey = encodeURIComponent(process.env.CONTENTFUL_API_KEY);
+const spaceId = encodeURIComponent(process.env.CONTENTFUL_SPACE_ID);
+const url = 'https://cdn.contentful.com/spaces/' + spaceId + '/entries?access_token=' + apiKey;
+
+const getSpace = url => {
+  return new Promise((resolve, reject) => {
+    request({
+      url: url,
+      json: true
+    }, (error, response, body) => {
+      if (error) {
+        reject('Unable to fetch space');
+      } else {
+        resolve(body.items);
+      }
+    });
+  });
+};
 
 
 // INDEX PAGE
 const index = pug.compileFile('templates/index.pug');
 const post = {
   title: 'Contentful — Github — Netlify',
-  content: 'Getting Netlify to pull repository from Github, manage the build and deploy to Github pages. And we have continuous deployment from Github set up.'
+  content: 'Getting Netlify to pull repository from Github, manage the build and deploy to Github pages. And we have continuous deployment from Github set up. But everything else is turning out to be a bitch.'
 };
 
-const data = index(post);
-fs.writeFileSync('.build/index.html', data);
+getSpace(url).then((items) => {
+  post.list = [];
+  items.forEach(i => post.list.push(i.fields.title));
+  fs.writeFileSync('.build/index.html', index(post));
+});

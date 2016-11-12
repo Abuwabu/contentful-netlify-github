e16101788f02cce059af13ca5d88d61fe2aebd54
pixels-bytes <adam@pixelsandbytes.co>
2016-11-12 10:42:23 +0100

    Init repo

---
+++ scripts/index.js
@@ -0,0 +1,34 @@
+/*jslint node: true, esversion: 6 */
+"use strict";
+
+
+/**
+ * CONTINUOUS DEPLOYMENT
+ * @version 1.0.0
+ * @author Pixels & Bytes
+ *
+ *         Getting Contentful — Github — Netlify all talking to eachother
+ *         Contentful: CaaS
+ *         GitHub: Repositories & Pages
+ *         Netlify: The build
+ *
+ * @requires fs
+ * @requires pug
+ */
+
+
+
+// THE REQUIREMENTS
+const fs = require('fs');
+const pug = require('pug');
+
+
+
+const index = pug.compileFile('index.pug');
+const post = {
+  title: 'Contentful — Github — Netlify',
+  content: 'Getting Netlify to pull repository from Github, manage the build and deploy to Github pages.'
+};
+
+const data = index(post);
+fs.writeFileSync('build/index.html', data);
